e4fbf8a06b01f22ac1a006c1e7c816efddee51c8
makam <makam@MSI>
2025-03-26 19:07:38 -0400

    AWS SDK3

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
-const AWS = require('aws-sdk');
+// const AWS = require('aws-sdk');
 const jwt = require('jsonwebtoken');
 const multerS3 = require('multer-s3');
 const multer = require('multer');
-const s3 = new AWS.S3();
+
+const { S3 } = require('@aws-sdk/client-s3');
+const s3Client = new S3({});
+
+const { S3Client, ListObjectsCommand } = require('@aws-sdk/client-s3');
+const s3 = new S3Client({ region: process.env.AWS_REGION || 'us-east-1' });
+
+const { CognitoIdentityServiceProvider } = require('@aws-sdk/client-cognito-identity-provider');
+const cognito = new CognitoIdentityServiceProvider({ region: process.env.COGNITO_REGION || 'us-east-1' });
+
+
 require('dotenv').config();
 
 const app = express();
@@ -14,11 +24,6 @@ let server;
 
 app.use(express.json());
 app.use(cors());
-
-// AWS Cognito config
-const cognito = new AWS.CognitoIdentityServiceProvider({
-    region: process.env.COGNITO_REGION || 'us-east-1',
-});
 
 const COGNITO_CLIENT_ID = process.env.COGNITO_CLIENT_ID || 'eoesr0bfd0n7i9l8t0vttgjff';
 
@@ -35,12 +40,9 @@ const pool = mysql.createPool({
 
 const upload = multer({
     storage: multerS3({
-        s3: s3,
+        s3: s3Client,
         bucket: process.env.S3_BUCKET_NAME || 'bucket-unify',
         acl: 'public-read',
-        metadata: function (req, file, cb) {
-            cb(null, { fieldName: file.fieldname });
-        },
         key: function (req, file, cb) {
             cb(null, `uploads/${Date.now()}_${file.originalname}`);
         }
@@ -48,17 +50,16 @@ const upload = multer({
 });
 
 // Test route to check S3 bucket connectivity
-app.get('/test-s3', (req, res) => {
-    s3.listObjects({ Bucket: 'bucket-unify' }, function(err, data) {
-      if (err) {
-        console.log(err, err.stack); // an error occurred
-        res.status(500).send('Error accessing S3: ' + err.message);
-      } else {
+app.get('/test-s3', async (req, res) => {
+    try {
+        const data = await s3.send(new ListObjectsCommand({ Bucket: 'bucket-unify' }));
         console.log(data); // successful response
         res.send('S3 Bucket Contents: ' + JSON.stringify(data));
-      }
-    });
-  });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Error accessing S3: ' + err.message);
+    }
+});
 
 app.post('/upload', upload.single('image'), (req, res) => {
     if (!req.file) {
