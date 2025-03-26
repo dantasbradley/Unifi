45453f90046119425508ad541bbc2c2c2c8fb0fe
makam <makam@MSI>
2025-03-26 18:23:49 -0400

    test upload pt2

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const mysql = require('mysql2');
 const AWS = require('aws-sdk');
 const jwt = require('jsonwebtoken');
 const multerS3 = require('multer-s3');
+const multer = require('multer');
 const s3 = new AWS.S3();
 require('dotenv').config();
 
@@ -35,7 +36,7 @@ const pool = mysql.createPool({
 const upload = multer({
     storage: multerS3({
         s3: s3,
-        bucket: process.env.S3_BUCKET_NAME,
+        bucket: process.env.S3_BUCKET_NAME || 'bucket-unify',
         acl: 'public-read',
         metadata: function (req, file, cb) {
             cb(null, { fieldName: file.fieldname });
