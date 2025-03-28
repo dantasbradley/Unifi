70a366bd6715a22f969770feff91a4ac72683085
dantasbradley <dantasbradley@gmail.com>
2025-03-28 15:01:17 -0400

    log fetch clubs

--- backend/index.js
+++ backend/index.js
@@ -478,11 +478,13 @@ app.get('/api/db-test', (req, res) => {
 
 // Function to retrieve all clubs
 app.get('/api/clubs', (req, res) => {
+    console.log('=== /api/clubs');
     pool.query('SELECT * FROM clubs', (err, results) => {
         if (err) {
             console.error('Error fetching clubs:', err);
             return res.status(500).json({ message: 'Failed to retrieve clubs' });
         }
+        console.log('Clubs fetched successfully:', results);
         res.json(results); // Send the retrieved clubs as a JSON response
     });
 });
