d8c57c57a5c2727d9bdd5f223f9100591d9d612f
shaynemarusic <92888014+shaynemarusic@users.noreply.github.com>
2025-04-06 15:52:46 -0400

    Doesn't matter; this is all getting reverted

--- backend/index.js
+++ backend/index.js
@@ -247,6 +247,7 @@ app.post('/login', (req, res) => {
 });
 
 // WIP
+// Function for handling notifications
 app.post('/notifications', async (req, res) => {
     console.log(`calling function: /date/${req.params.date}`);
     const { sub } = req.body;
