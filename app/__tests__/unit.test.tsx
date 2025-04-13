6101710b7df2a041774d84b19412210c353cffed
shaynemarusic <92888014+shaynemarusic@users.noreply.github.com>
2025-04-13 17:46:34 -0400

    More tests: login page ui tests

--- app/__tests__/unit.test.tsx
+++ app/__tests__/unit.test.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { fireEvent, render, waitFor } from "@testing-library/react-native";
+import { fireEvent, render, userEvent, waitFor } from "@testing-library/react-native";
 import { renderRouter, screen } from 'expo-router/testing-library';
 import AuthScreen from "../screens/AuthScreen";
 import Login from "../screens/Login";
 import { ReactTestInstance } from "react-test-renderer";
 import { press } from "@testing-library/react-native/build/user-event/press";
 import SignUp from "../screens/SignUp";
+import Toast from "react-native-toast-message";
 
 jest.mock("@react-native-async-storage/async-storage", () => {
     require('@react-native-async-storage/async-storage/jest/async-storage-mock');
@@ -44,6 +45,12 @@ describe("Front End Tests", () => {
 
     // Login Page Tests
     describe("Login Page Tests", () => {
+
+        let fetchMock = jest.spyOn(global, "fetch").mockImplementation(jest.fn());
+
+        beforeEach(() => {
+            jest.clearAllMocks();
+        })
         
         it("Default text and buttons render correctly", () => {
             render(<Login/>);
@@ -56,21 +63,101 @@ describe("Front End Tests", () => {
             expect(screen.getByText("Forgot password?")).toBeOnTheScreen();
             expect(screen.queryByTestId("Activity Indicator")).not.toBeOnTheScreen();
             expect(screen.queryByRole("Toast")).not.toBeOnTheScreen();
-
-            
         })
 
-        it("Toast messages appear correctly", async () => {
+        it("Missing Fields toast message renders correctly with no fields filled", async () => {
             render(<Login/>);
 
             const loginButton = screen.getByText("Login");
+            const emailField = screen.getByPlaceholderText("Enter your email");
+            const passwordField = screen.getByPlaceholderText("Enter your password");
+
+            fireEvent.press(loginButton);
+            
+            // Make sure the missing fields toast message appears
+            expect(await screen.findByText("Missing Fields")).toBeTruthy();
+        })
+
+        it("Missing Fields toast message renders correctly with just email field filled", async () => {
+            render(<Login/>);
+
+            const loginButton = screen.getByText("Login");
+            const emailField = screen.getByPlaceholderText("Enter your email");
+
+            await userEvent.type(emailField, "test");
+            fireEvent.press(loginButton);
+
+            expect(await screen.findByText("Missing Fields")).toBeTruthy();
+        })
+
+        it("Missing Fields toast message renders correctly with just password field filled", async () => {
+            render(<Login/>);
+
+            const loginButton = screen.getByText("Login");
+            const passwordField = screen.getByPlaceholderText("Enter your password");
+
+            await userEvent.type(passwordField, "test");
+            fireEvent.press(loginButton);
+
+            expect(await screen.findByText("Missing Fields")).toBeTruthy();
+        })
+
+        it("Login Failed toast messages render correctly when presented with invalid credentials", async () => {
+            fetchMock.mockReturnValueOnce(Promise.resolve({status: 401, ok: false, json: () => Promise.resolve({})} as Response));
+
+            render(<Login/>);
+
+            const loginButton = screen.getByText("Login");
+            const emailField = screen.getByPlaceholderText("Enter your email");
+            const passwordField = screen.getByPlaceholderText("Enter your password");
+
+            await userEvent.type(emailField, "test1");
+            await userEvent.type(passwordField, "test2");
 
             fireEvent.press(loginButton);
 
-            // Make sure the missing fields toast message appears
-            await waitFor(async () => {
-                expect(await screen.findByText("Missing Fields")).toBeTruthy();
-            })
+            expect(fetchMock).toHaveBeenCalledWith("http://3.85.25.255:3000/login", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ email: "test1".trim(), password: "test2" })})
+
+            expect(await screen.findByText("Invalid email or password.")).toBeTruthy();
+        })
+
+        it ("Login Failed toast message renders correctly when presented with unexpected status code", async () => {
+            fetchMock.mockReturnValueOnce(Promise.resolve({status: 402, ok: false, json: () => Promise.resolve({})} as Response));
+
+            render(<Login/>);
+
+            const loginButton = screen.getByText("Login");
+            const emailField = screen.getByPlaceholderText("Enter your email");
+            const passwordField = screen.getByPlaceholderText("Enter your password");
+
+            await userEvent.type(emailField, "test1");
+            await userEvent.type(passwordField, "test2");
+
+            fireEvent.press(loginButton);
+
+            expect(await screen.findByText("Something went wrong.")).toBeTruthy();
+        })
+
+        it ("Login Succeeded toast message renders correctly", async () => {
+            fetchMock.mockReturnValueOnce(Promise.resolve({ok: true, json: () => Promise.resolve({})} as Response));
+
+            render(<Login/>);
+
+            const loginButton = screen.getByText("Login");
+            const emailField = screen.getByPlaceholderText("Enter your email");
+            const passwordField = screen.getByPlaceholderText("Enter your password");
+
+            await userEvent.type(emailField, "test1");
+            await userEvent.type(passwordField, "test2");
+
+            fireEvent.press(loginButton);
+
+            expect(await screen.findByText("Success")).toBeTruthy();
         })
 
     })
