a98d28e1194fc8d15da4994d99541d23e155c063
dantasbradley <dantasbradley@gmail.com>
2025-04-14 11:31:15 -0400

    post with pic pt1

--- app/tabs/ExploreScreen/CommunityDetails.tsx
+++ app/tabs/ExploreScreen/CommunityDetails.tsx
@@ -26,6 +26,8 @@ export default function CommunityDetailsScreen() {
     fetchPostsForClub = () => {},
     fetchEventsForClub = () => {},
     fetchClubImage = () => {},
+    fetchPostImage = () => {},
+    uploadImage = () => {},
   } = useContext(CommunitiesContext) || {};
 
   const [activeTab, setActiveTab] = useState<"Bio" | "Events" | "Community">("Bio");
@@ -151,20 +153,24 @@ export default function CommunityDetailsScreen() {
   
 
   // Return white color if tab is active, else gray
-  const handleCreatePost = async () => {
+  const handleCreatePost = async (imageUri: string | null) => {
     if (!newPostName || !newPostContent) {
         Alert.alert("Error", "Title and content are required.");
         return;
     }
 
     try {
+        let filePath = "";
+        if (imageUri) {
+          filePath = "yes";
+        }
         const response = await fetch("http://3.85.25.255:3000/DB/posts/add", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
                 title: newPostName,
                 content: newPostContent,
-                filePath: "",
+                filePath,
                 club_id: id,
             }),
         });
@@ -172,6 +178,11 @@ export default function CommunityDetailsScreen() {
 
         if (response.ok) {
             await handleRefreshPosts();
+            console.log("Message result:", data.message);
+            if (imageUri) {
+              console.log("uploading image: ", data.filePath);
+              uploadImage(imageUri, data.filePath);
+            }
             setPostModalVisible(false);
             setNewPostName("");
             setNewPostContent("");
@@ -345,7 +356,7 @@ export default function CommunityDetailsScreen() {
     setImageUrl(imageUrl); 
   };
 
-  const uploadImage = async (filePath : any, imageUri : any) => {
+  const uploadClubImage = async (filePath : any, imageUri : any) => {
     try {
         console.log("filePath: ", filePath);
         const response = await fetch(`http://3.85.25.255:3000/S3/get/upload-signed-url?filePath=${encodeURIComponent(filePath)}`);
@@ -379,7 +390,7 @@ export default function CommunityDetailsScreen() {
     });
 
     if (!result.canceled) {
-        uploadImage(`club_profile_pics/${id}_${name}`, result.assets[0].uri);
+        uploadClubImage(`club_profile_pics/${id}_${name}`, result.assets[0].uri);
     }
   };
 
