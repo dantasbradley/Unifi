68a8fae6f85f0fba5367cacd1c3ac6f877a9dbb8
ChristopherBarroso <cbarroso1@ufl.edu>
2025-04-20 19:33:54 -0400

    Updated Layout.tsx
    -Changed Header formats for login/signup pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,12 +6,26 @@ export default function Layout() {
     <HamburgerProvider> 
       <Stack>
         <Stack.Screen name="index" options={{ title: "Home" }} />
-        <Stack.Screen name="screens/AuthScreen" options={{ title: "Auth" }} />
-        <Stack.Screen name="screens/Login" options={{ title: "Login" }} />
-        <Stack.Screen name="screens/SignUp" options={{ title: "Sign Up" }} />
+        <Stack.Screen name="screens/AuthScreen" options={{ headerShown: false }} />
+
+        <Stack.Screen
+          name="screens/Login"
+          options={{
+            title: "Login",
+            headerStyle: { backgroundColor: "#344E41" },
+            headerTintColor: "#fff",
+          }}
+        />
+        <Stack.Screen
+          name="screens/SignUp"
+          options={{
+            title: "Sign Up",
+            headerStyle: { backgroundColor: "#344E41" },
+            headerTintColor: "#fff",
+          }}
+        />
+
         <Stack.Screen name="screens/Verification" options={{ title: "Verification" }} />
-
-
         <Stack.Screen name="tabs" options={{ headerShown: false }} />
       </Stack>
     </HamburgerProvider>
